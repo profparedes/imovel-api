d3ad0e2c8cc8402a182a81f28f4f9b4b2f764ad5
Fábio Paredes <f.paredes.o@gmail.com>
2022-12-08 10:59:21 -0300

    removing index, show and delete of PictureController

--- app/Http/Controllers/PictureController.php
+++ app/Http/Controllers/PictureController.php
@@ -8,15 +8,7 @@ use Illuminate\Http\Response;
 
 class PictureController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return response()->json(Picture::get());
-    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -32,30 +24,6 @@ class PictureController extends Controller
         );
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Picture  $picture
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Picture $picture)
-    {
-        return response()->json($picture);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Picture  $picture
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Picture $picture)
-    {
-        $picture->update($request->all());
-
-        return response()->json($picture);
-    }
 
     /**
      * Remove the specified resource from storage.
